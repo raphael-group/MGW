d16b00c90ea5da2fb0b48b6460fc0e90384c7e86
Yonghao Zhao <118432288+YonghaoZhao722@users.noreply.github.com>
2026-05-08 11:55:37 +0800

    Disable JAX GPU preallocation by default

--- mgw/__init__.py
+++ mgw/__init__.py
@@ -1,3 +1,4 @@
+from . import _jax_config  # noqa: F401 - configure JAX before any OTT/JAX imports
 from .models import PhiModel, PhiModelFFN, train_phi, train_phi_pro
 from .geometry import pullback_metric_field, knn_graph, geodesic_distances, pairwise_squared_geodesic
 from .gw import gw_distance, solve_gw
